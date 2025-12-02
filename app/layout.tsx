6053917ca2787d579fb62825e852a6d1bd291240
Vikas Sahu <40103679+vxsahu@users.noreply.github.com>
2025-12-02 18:41:31 +0530

    okkkk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./animate.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import LayoutContent from "@/components/LayoutContent";
--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,5 +1,4 @@
-import { mergeProps } from "@base-ui-components/react/merge-props";
-import { useRender } from "@base-ui-components/react/use-render";
+import { mergeProps, useRender } from "@base-ui-components/react";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
